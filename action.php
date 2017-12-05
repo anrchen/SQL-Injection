d84d901ade44311487f090a9c55ae3f2ea2e8651
anrchen <anr_chen@live.concordia.ca>
2017-12-04 21:57:07 -0500

    Added action.php

--- action.php
+++ action.php
@@ -1,6 +1,15 @@
 <?php
     if ($_POST['defense']=="prepared_statement"){
         echo("Hello world");
+
     }
 
+$mysqli = new mysqli('localhost', 'root', '', 'soen321');
+$rs = $mysqli->query( 'CALL procedureLogin("soen1232131321", "123")' );
+
+if ($rs){
+    $actor = $rs->fetch_assoc();;
+    echo ("Hello ".$actor);
+    echo ("The data exists");
+}
 ?>
